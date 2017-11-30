a34e68c16e8a88f880ddfe0ffebc4666bb4d84d7
Lukasz Bacik <lukasz.bacik@performgroup.com>
2017-11-30 21:29:57 +0100

    Updated rpi-php-apache

--- rpi-php-apache/build.sh
+++ rpi-php-apache/build.sh
@@ -2,21 +2,27 @@
 
 DOCKER_BUILDER=docker-image-builder
 
-while getopts "h?H:" opt; do 
+while getopts "h?H:p:" opt; do 
 	case "$opt" in
-	h\?)
+		h\?)
 			show_help
 			exit 0
 			;;
-	H)	HOST="-H $OPTARG"
-			;;				
+		H)	HOST="-H $OPTARG"
+			;;
+		p)	DOCKERFILE="$OPTARG"
+			;;
 	esac
 done
+
+if [ -z "$PATH" ]; then
+    echo "Option -p (PATH) not specified"
+    exit 1
+fi
 
 ${DOCKER_BUILDER} ${HOST} \
 		--images-name-prefix "build-php-" \
 		--final-image-name "php:build" \
 		--remove-builds \
 		first \
-		php/5.6/apache
-	
+		${DOCKERFILE}
